867faf05f28a886ae5bd0c5402ce09db89870338
killi8n <killi8n.dev@gmail.com>
2020-11-01 14:25:16 +0900

    feat: published to npm

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 module.exports = function(api) {
     api.cache(true);
     return {
-      presets: ['module:metro-react-native-babel-preset'],
+      presets: [
+        ['module:metro-react-native-babel-preset'],
+        [
+          '@babel/preset-env',
+          {
+            modules: process.env.BABEL_ENV === 'esm' ? false : 'commonjs',
+            targets: {
+              ie: '11',
+            },
+          }
+        ]
+      ],
       plugins: [
           'react-native-web',
           ["module-resolver", {
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as NewsItem } from "./NewsItem"
+export { default as TabBar } from "./TabBar"
+export { default as TabBarButton } from "./TabBarButton"
