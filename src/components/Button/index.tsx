5dfc62f2839ebf5cf15a5a3e9932fcb882096593
killi8n <killi8n.dev@gmail.com>
2020-11-08 00:59:02 +0900

    feat: modified Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,12 @@ import React, { FC } from 'react';
 import { BaseToucableOpacity } from './styled';
 import { ButtonProps } from './types';
 
-const Button: FC<ButtonProps> = ({ children, rounded, customStyles }) => {
+const Button: FC<ButtonProps> = ({ children, rounded, customStyles, onPress }) => {
     return (
         <BaseToucableOpacity
             style={customStyles}
             rounded={rounded}
+            onPress={onPress}
         >
             {children}
         </BaseToucableOpacity>
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -3,6 +3,7 @@ import { StyleProp, ViewStyle } from 'react-native';
 export interface ButtonProps {
     rounded?: boolean;
     customStyles?: StyleProp<ViewStyle>
+    onPress?: () => Promise<void> | void
 }
 
 export interface BaseToucableOpacityProps {
